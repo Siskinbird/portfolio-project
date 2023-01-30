79f5ac6d0f47432e91eeb0a6b5f68c2a2fc6d573
Dmitrii Chizhov <dimachig@gmail.com>
2023-01-30 12:27:50 +0300

    add: hide preloader function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+setTimeout(function() {
+    let preloader = document.getElementById("preloader");
+    preloader.style.opacity = 0;
+    setTimeout(function() {
+        preloader.style.display = "none";
+    }, 1000);
+}, 5000);
+
 console.log('My App!');
