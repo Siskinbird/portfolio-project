f9d837b9bf6a898e9ee5675c78e6fe264610b7a6
Dmitrii Chizhov <dimachig@gmail.com>
2023-01-30 18:58:32 +0300

    add: all sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 setTimeout(function() {
     let preloader = document.getElementById("preloader");
+    let wrapper = document.getElementById('wrapper')
     preloader.style.opacity = 0;
     setTimeout(function() {
         preloader.style.display = "none";
+        wrapper.style.display = "block";
+        wrapper.style.opacity = 1;
     }, 1000);
 }, 5000);
 
